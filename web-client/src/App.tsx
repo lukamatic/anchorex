6babbb3a21dcc00b1e4691c4ae60a116349f6df6
lukamatic <matic.ra16.2018@uns.ac.rs>
2021-11-02 00:13:40 +0100

    Configured react-router-dom

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Signup from './components/Signup';
 import Welcome from './components/Welcome';
 
 function App() {
   return (
-    <div>
-      <Welcome />
-    </div>
+    <Router>
+      <Route path='/' exact>
+        <Welcome />
+      </Route>
+      <Route path='/signup'>
+        <Signup />
+      </Route>
+    </Router>
   );
 }
 

a87012c95abde0d8870ed9b8d84459731fc2f614
Petar Obradovic <ns.o.petar@gmail.com>
2022-01-07 18:44:50 +0100

    feat: DatePicker utils

---
+++ web-client/src/utils/dateUtils.ts
@@ -0,0 +1,25 @@
+export const addDays = (date: Date, days:number):Date => {
+    return new Date(date.setDate(date.getDate() + days));
+}
+
+export const convertDateToStringForPicker = (date:Date|string|undefined):string|undefined => { 
+    if(date === undefined)return date;
+    try{
+        var d = new Date(date as string),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+        
+        if (month.length < 2) 
+        month = '0' + month;
+        if (day.length < 2) 
+        day = '0' + day;
+        return "" + [year, month, day].join('-');
+    }catch(e){
+        return date as string|undefined;
+    }
+}
+
+export const convertStringToDateFromPicker = (date:string):Date => {
+    return new Date(date);
+}

9a57fdc1f3b15cf54503063ef2ea2edc7c04413a
ogijah <bogdanovic.ra101.2018@uns.ac.rs>
2022-05-25 12:16:00 +0200

    feat: actions for clients

--- server/anchorex/src/main/java/com/teameleven/anchorex/repository/ReservationRepository.java
+++ server/anchorex/src/main/java/com/teameleven/anchorex/repository/ReservationRepository.java
@@ -11,7 +11,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Query(value = "SELECT r FROM Reservation r where r.ownerId=?1 and r.captain=true")
     List<Reservation> getOwnerReservations(Long id);
 
-    @Query(value = "SELECT r FROM Reservation r where r.reservationEntityId=?1")
+    @Query(value = "SELECT r FROM Reservation r where r.reservationEntityId=?1 and r.userId=null")
     List<Reservation> getEntityReservations(Long id);
 
 }
--- server/anchorex/src/main/java/com/teameleven/anchorex/service/ReservationService.java
+++ server/anchorex/src/main/java/com/teameleven/anchorex/service/ReservationService.java
@@ -3,8 +3,14 @@ package com.teameleven.anchorex.service;
 import com.teameleven.anchorex.domain.Reservation;
 import com.teameleven.anchorex.dto.DateRangeDTO;
 import com.teameleven.anchorex.dto.ReservationDTO;
+import com.teameleven.anchorex.dto.reservationentity.ClientReservationDTO;
+
+import java.util.List;
 
 public interface ReservationService {
     Reservation createReservation(ReservationDTO reservationDTO);
+
     boolean checkCaptainAvailability(DateRangeDTO dateRangeDTO);
+
+    List<ClientReservationDTO> getFreeReservations(Long id);
 }

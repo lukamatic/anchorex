6f2d54440ad2ad1b996bbd381e2ce9aa3a6c4ec1
Luka Matić <matic.ra16.2018@uns.ac.rs>
2022-06-06 23:50:40 +0200

    Update Login.tsx

--- web-client/src/components/login/Login.tsx
+++ web-client/src/components/login/Login.tsx
@@ -9,139 +9,150 @@ import localStorageUtil from '../../utils/local-storage/local-storage-util';
 import LoadingSpinner from '../common/LoadingSpinner';
 
 const Login = () => {
-	const authContext = useContext(AuthContext);
-	const history = useHistory();
+  const authContext = useContext(AuthContext);
+  const history = useHistory();
 
-	const [fetching, setFetching] = useState(false);
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [errorLabelText, setErrorLabelText] = useState('');
+  const [fetching, setFetching] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorLabelText, setErrorLabelText] = useState('');
 
-	const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setEmail(event.target.value);
-	};
+  const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
 
-	const passwordChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setPassword(event.target.value);
-	};
+  const passwordChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPassword(event.target.value);
+  };
 
-	const precreatedUsers = [
-		{
-			role: 'lodge-ownera',
-			data: { email: 'bogdanovicognjen@gmail.com', password: 'bbogi1219' },
-		},
-		{
-			role: 'instructor',
-			data: { email: 'luka.mat99@gmail.com', password: 'luka123' },
-		},
-		{
-			role: 'ship-ownera',
-			data: { email: 'sreckosojic@gmail.com', password: 'brod123' },
-		},
-		{
-			role: 'client',
-			data: { email: 'client@example.com', password: 'client123' },
-		},
-	];
+  const precreatedUsers = [
+    {
+      role: 'lodge-ownera',
+      data: { email: 'bogdanovicognjen@gmail.com', password: 'bbogi1219' },
+    },
+    {
+      role: 'instructor',
+      data: { email: 'luka.mat99@gmail.com', password: 'luka123' },
+    },
+    {
+      role: 'ship-ownera',
+      data: { email: 'sreckosojic@gmail.com', password: 'brod123' },
+    },
+    {
+      role: 'client',
+      data: { email: 'client@example.com', password: 'client123' },
+    },
+  ];
 
-	const doLogin = async (data: { email: string; password: string }) => {
-		const url: string = '/api/auth/login';
-		setFetching(true);
+  const doLogin = async (data: { email: string; password: string }) => {
+    const url: string = '/api/auth/login';
+    setFetching(true);
 
-		const response = await fetch(url, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(data),
-		});
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
 
-		switch (response.status) {
-			case HttpStatusCode.OK:
-				setErrorLabelText('');
+    switch (response.status) {
+      case HttpStatusCode.OK:
+        setErrorLabelText('');
 
-				var content = await response.json();
+        var content = await response.json();
 
-				const user: AuthContextUser = {
-					accessToken: content.userTokenState.accessToken,
-					loggedIn: true,
-					id: content.userId,
-					email: data.email,
-					role: content.userRole,
-				};
+        const user: AuthContextUser = {
+          accessToken: content.userTokenState.accessToken,
+          loggedIn: true,
+          id: content.userId,
+          email: data.email,
+          role: content.userRole,
+        };
 
-				authContext.updateAuthContext(user);
-				localStorageUtil.setUser(user);
+        authContext.updateAuthContext(user);
+        localStorageUtil.setUser(user);
 
-				const resp = await getUserByTokenAsync();
-				if (resp.status === HttpStatusCode.OK) {
-					authContext.setUserDetails(resp.data);
-				} else {
-					localStorage.clear();
-				}
+        const resp = await getUserByTokenAsync();
+        if (resp.status === HttpStatusCode.OK) {
+          authContext.setUserDetails(resp.data);
+        } else {
+          localStorage.clear();
+        }
 
-				if (localStorageUtil.getUserRole() === UserRole.LODGE_OWNER) {
-					history.push('/lodges');
-				} else if (localStorageUtil.getUserRole() === UserRole.SHIP_OWNER) {
-					history.push('/ships');
-				} else {
-					history.push('/');
-				}
-				break;
-			case HttpStatusCode.UNAUTHORIZED:
-				setErrorLabelText('Invalid credentials!');
-				break;
-			case HttpStatusCode.FORBIDDEN:
-				setErrorLabelText('');
-				history.push('/verification');
-				break;
-			default:
-				setErrorLabelText('Unknown error occurred.');
-				break;
-		}
+        if (localStorageUtil.getUserRole() === UserRole.LODGE_OWNER) {
+          history.push('/lodges');
+        } else if (localStorageUtil.getUserRole() === UserRole.SHIP_OWNER) {
+          history.push('/ships');
+        } else {
+          history.push('/');
+        }
+        break;
+      case HttpStatusCode.UNAUTHORIZED:
+        setErrorLabelText('Invalid credentials!');
+        break;
+      case HttpStatusCode.FORBIDDEN:
+        setErrorLabelText('');
+        history.push('/verification');
+        break;
+      default:
+        setErrorLabelText('Unknown error occurred.');
+        break;
+    }
 
-		setFetching(false);
-	};
+    setFetching(false);
+  };
 
-	const logIn = async (e: any) => {
-		e.preventDefault();
+  const logIn = async (e: any) => {
+    e.preventDefault();
 
-		const data = { email: email, password: password };
-		doLogin(data);
-	};
+    const data = { email: email, password: password };
+    doLogin(data);
+  };
 
-	return (
-		<div className='flex flex-col items-center flex-grow bg-blue-50 p-3'>
-			<form onSubmit={logIn} className='flex flex-col bg-white w-full md:w-80 md:text-lg mt-12 md:mt-40 m-5 p-5 rounded-lg shadow-lg'>
-				<input className='input mt-3 mb-2' type='email' name='email' id='email' placeholder='Enter email' onChange={emailChangeHandler} />
-				<input className='input' type='password' name='password' id='password' placeholder='Enter password' onChange={passwordChangeHandler} />
-				<p className='self-center text-red-500 text-center w-52 mt-3 mb-3'>{errorLabelText}</p>
-				{fetching ? (
-					<div className='flex justify-center items-center mb-3'>
-						<LoadingSpinner />
-					</div>
-				) : (
-					<button className='btnBlueWhite mb-3' type='submit'>
-						Log in
-					</button>
-				)}
-			</form>
-			<Link className='btnWhiteBlue mb-3 text-center' to='/signupChoice'>
-				Sign up
-			</Link>
-			{/* </form> */}
-
-			{precreatedUsers.map((user: any) => (
-				<div
-					onClick={() => {
-						doLogin(user.data);
-					}}
-				>
-					{user.role}
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className='flex flex-col items-center flex-grow bg-blue-50 p-3'>
+      <form
+        onSubmit={logIn}
+        className='flex flex-col bg-white w-full md:w-80 md:text-lg mt-12 md:mt-40 m-5 p-5 rounded-lg shadow-lg'
+      >
+        <input
+          className='input mt-3 mb-2'
+          type='email'
+          name='email'
+          id='email'
+          placeholder='Enter email'
+          onChange={emailChangeHandler}
+        />
+        <input
+          className='input'
+          type='password'
+          name='password'
+          id='password'
+          placeholder='Enter password'
+          onChange={passwordChangeHandler}
+        />
+        <p className='self-center text-red-500 text-center w-52 mt-3 mb-3'>
+          {errorLabelText}
+        </p>
+        {fetching ? (
+          <div className='flex justify-center items-center mb-3'>
+            <LoadingSpinner />
+          </div>
+        ) : (
+          <button className='btnBlueWhite mb-3' type='submit'>
+            Log in
+          </button>
+        )}
+      </form>
+      <Link className='btnWhiteBlue mb-3 text-center' to='/signupChoice'>
+        Sign up
+      </Link>
+      {/* </form> */}
+    </div>
+  );
 };
 
 export default Login;
